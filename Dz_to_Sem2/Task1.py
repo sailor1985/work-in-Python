6a4531c6de4edd69398fd8319f1a97f4ac4795d2
Michael <cadetvire@rambler.ru>
2022-10-11 01:11:19 +0300

    Есть решение суммы цифр для вещественного числа

--- Dz_to_Sem2/Task1.py
+++ Dz_to_Sem2/Task1.py
@@ -5,14 +5,20 @@
 # - 0,56     -> 11
 # - 187,6778 -> 44
 
-number = float(input(f'Введите число N: '))
+number = input(f'Введите число N\n В случае ввода вещественного числа используйте ".": ')
 
-def sum_of_digits(n):
+number_split = number.split(".")
+number_int = int(number_split[0]) # выделение целой части
+number_float = int(number_split[1]) # выделение дробной части
+
+def sum_of_digits(n): #функция нахождения суммы цифр в числе
     sum = 0
     while n != 0: 
         sum += n % 10
         n //= 10 
     return sum
 
-sum_digits = int(sum_of_digits(number))
-print(f"Сумма цифр в числе {number} равна {sum_digits}")
+sum_int = sum_of_digits(number_int)   #нашли суммы цифр в целой части числа
+sum_float = sum_of_digits(number_float)   #нашли суммы цифр в дробной части числа
+sum_finish = sum_int + sum_float   #общая суммы цифр в числе
+print(f"Сумма цифр в числе {number} равна {sum_finish}")
