6a8ed616f5fdbd748ad418655275d67ccbbc1c1e
Michael <cadetvire@rambler.ru>
2022-10-11 21:06:59 +0300

    Окончательная финишная версия программы

--- Dz_to_Sem2/Task1.py
+++ Dz_to_Sem2/Task1.py
@@ -5,20 +5,12 @@
 # - 0,56     -> 11
 # - 187,6778 -> 44
 
-number = input(f'Введите число N\n В случае ввода вещественного числа используйте ".": ')
+number = float(input(f"Введите число N\n В случае ввода вещественного числа используйте точку: "))
 
-number_split = number.split(".")
-number_int = int(number_split[0]) # выделение целой части
-number_float = int(number_split[1]) # выделение дробной части
-
-def sum_of_digits(n): #функция нахождения суммы цифр в числе
-    sum = 0
-    while n != 0: 
-        sum += n % 10
-        n //= 10 
-    return sum
-
-sum_int = sum_of_digits(number_int)   #нашли суммы цифр в целой части числа
-sum_float = sum_of_digits(number_float)   #нашли суммы цифр в дробной части числа
-sum_finish = sum_int + sum_float   #общая суммы цифр в числе
-print(f"Сумма цифр в числе {number} равна {sum_finish}")
+str_number = str(number) # перевод из типа float в тип str
+str_number = str_number.replace('.', '')  # производим замену десятичного разделителя
+lst_str = list(str_number)   # перевод строки с числом в список строк с цифрами
+lst_num = map(int, lst_str)   # преобразовываем каждый элемент полученного
+                              # списка строк с цифрами в список целых чисел
+sum_number = sum(lst_num)  # применяем функцию `sum()`
+print(f"Сумма цифр в числе {number} равна {sum_number}")
