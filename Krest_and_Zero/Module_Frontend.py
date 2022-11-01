440883777458d2b82a091e9601804701d310941f
Michael <cadetvire@rambler.ru>
2022-11-02 00:59:35 +0300

    Создание frontend модуля

--- Krest_and_Zero/Module_Frontend.py
+++ Krest_and_Zero/Module_Frontend.py
@@ -1,5 +1,6 @@
-import Module_rendering_function_with_Texttable as render
+# Создание frontend модуля: все, что связано с прорисовкой поля
 import Module_data_structure as str
+import Module_rendering_function_with_Texttable as render
 
-dictinary = str.structure() #Подтянули структуру данных - словарь 
-render.rendering(dictinary) #Запустили функцию отрисовки поля клеток с рандомными значениями: "X", "", "0" 
+dictinary = str.structure() #Вызов функции заполнения структуры данных (словарь)
+render.rendering(dictinary) #Вызов функции отрисовки поля клеток с рандомными значениями: "X", "", "0" 
---
+++ Krest_and_Zero/Module_rendering_function_with_Texttable.py
@@ -0,0 +1,8 @@
+def rendering(dictinary: dict): #Функция рисование поля клеток с отображением
+                                #текущего состояния (frontend в консоли)
+    from texttable import Texttable
+
+    lst = list(dictinary.values()) #Вытащили из словаря все значения в список
+    table = Texttable() 
+    table.add_rows([["1", "2", "3"], lst[:3], lst[3:6], lst[6:9]])
+    print(table.draw())
