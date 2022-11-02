b46a687edb5e39a8139994ab1bc36a4d932af6dc
Michael <cadetvire@rambler.ru>
2022-11-02 23:12:06 +0300

    В функциях везде вынес импорт за ее пределы

--- Krest_and_Zero/Module_data_structure_dictionary.py
+++ Krest_and_Zero/Module_data_structure_dictionary.py
@@ -1,6 +1,6 @@
-def struct_dic(): # Структура хранения данных (словарь) и его случайное заполнение
-    from random import choice
+from random import choice
 
+def struct_dic(): # Структура хранения данных (словарь) и его случайное заполнение
     structure_dic = dict.fromkeys(['x1y1','x1y2','x1y3','x2y1','x2y2','x2y3','x3y1','x3y2','x3y3' ], '')
     for key in structure_dic:
         structure_dic[key] = choice(["X", "0", ""])
--- Krest_and_Zero/Module_rendering_function_with_Texttable.py
+++ Krest_and_Zero/Module_rendering_function_with_Texttable.py
@@ -1,7 +1,7 @@
+from texttable import Texttable
+
 def rendering(dictinary: dict): #Функция рисование поля клеток с отображением
                                 #текущего состояния (frontend в консоли)
-    from texttable import Texttable
-    
     lst = list(dictinary.values()) #Вытащили из словаря все значения в список
     table = Texttable() 
     table.add_rows([["1", "2", "3"], lst[:3], lst[3:6], lst[6:9]])
