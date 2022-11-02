ac2ae005fa9a8d84114bdae7f91f862ee7627c48
Michael <cadetvire@rambler.ru>
2022-11-02 22:59:14 +0300

    Корректировка №3 этапа 1 после замечаний

--- Krest_and_Zero/Module_Frontend.py
+++ Krest_and_Zero/Module_Frontend.py
@@ -1,6 +1,8 @@
 # Создание frontend модуля: все, что связано с прорисовкой поля
-import Module_data_structure as str
-import Module_rendering_function_with_Texttable as render
 
-dictinary = str.structure() #Вызов функции заполнения структуры данных (словарь)
-render.rendering(dictinary) #Вызов функции отрисовки поля клеток с рандомными значениями: "X", "", "0" 
+import Module_rendering_function_with_Texttable as render
+import Module_data_structure_dictionary as D
+
+#Вызов функции отрисовки поля клеток, принимающей текущее состояние системы:
+#рандомные значения: "X", "", "0"
+render.rendering(D.struct_dic()) 
