da72ab352a9d32575043f1bb4333ead0bbc1ec9b
Michael <cadetvire@rambler.ru>
2022-11-09 20:47:17 +0300

    Добавил функцию кооретности ввода данных. В ней же функция заполнения поля клеток х или 0

---
+++ Krest_and_Zero/Module_correctness_input_data.py
@@ -0,0 +1,23 @@
+from Module_rendering_function_with_Texttable import step_maps
+
+def valid_input(current_gamer, maps, value):
+    valid = False
+    while not valid:
+        step = input(f"{current_gamer.capitalize()}, выберите ячейку, в которую хотите поставить ваш знак: ")
+        try:
+            step = int(step)
+        except:
+            print("Некорректный ввод. Вы уверены, что ввели число?") 
+            continue
+        if step >= 1 and step <= 9:
+            if (str(maps[step-1]) not in "x0"):
+                step_maps(maps,step,value) # Функция заполнения списка крестиком или ноликом,
+                                            # в зависимости от того что в нее передали.
+                valid = True
+            else:
+                print("Эта клеточка уже занята")
+                continue
+        else:
+                print("Некорректный ввод. Введите число от 1 до 9 чтобы сделать ход.")
+                continue
+        
--- Krest_and_Zero/Module_tic_tac_toe_2_players.py
+++ Krest_and_Zero/Module_tic_tac_toe_2_players.py
@@ -1,5 +1,6 @@
 from random import choice
-from Module_rendering_function_with_Texttable import rendering_list, step_maps
+from Module_rendering_function_with_Texttable import rendering_list
+from Module_correctness_input_data import valid_input
 import keyboard
 
 def two_players(number_of_cells = 9): #Функция игры 2 игроков
@@ -9,50 +10,31 @@ def two_players(number_of_cells = 9): #Функция игры 2 игроков
     print("\nСейчас начнется жеребьевка за право первого хода!\n\n"
      f"Поздравляем, {current_gamer.capitalize()}, право первого хода предоставлено вам!\n")
     
-    maps = list(range(1,10)) #Структура хранения данных (список), заполненная цифрами в диапозоне от 1 до 9,
-                             #которые являются вводимыми данным игроков для того, чтобы показать в какую ячейку
+    maps = list(range(1,10)) #Структура хранения данных (список), заполненная цифрами в диапозоне от 1 до 9.
+                             #Игрок вводит цифру из этого диапозона тем самым показывая в какую ячейку
                              #поставить свой знак: x или 0
 
     print("Текущее состояние поля клеток")
-    rendering_list(maps) # Показываем исходное состояние поля клеток,
-                         # используя функцию рисования поля клеток с отображением
-                         # текущего состояния
-
+    rendering_list(maps) # Функция рисования исходного состояния поля клеток
+  
     while number_of_cells != 0:
         print(f"Количество оставшихся ячеек: {number_of_cells}")
-        valid = False
         count = 0
         print("Чтобы прервать игру нажмите клавишу q, продолжить - пробел: ")
-        if keyboard.read_key() == "q": #Функция прерывания игры игроком
+        if keyboard.read_key() == "q": #Функция прерывания игроком игры ее начала
             print("Игра прервана игроком")
             break
-        while not valid:
-            if keyboard.read_key() == "q":
-                print("Игра прервана игроком")
-                break
-            if current_gamer == gamer_1: value = "x"
-            else: value = "0"
-            step = input(f"{current_gamer.capitalize()}, выберите ячейку, в которую хотите поставить ваш знак: ")
+ 
+        if current_gamer == gamer_1: value = "x"
+        else: value = "0"
+        if keyboard.read_key() == "q": #Функция прерывания игроком игры до своего хода
+            print("Игра прервана игроком")
+            break
 
-            # Проверка корректности ввода номера ячейки (ввод строки, пустого значения, значения не из диапозона от 1 до 9,
-            # а также проверка условия занятости ячейки
-            try:
-                step = int(step)
-            except:
-                print("Некорректный ввод. Вы уверены, что ввели число?") 
-                continue
-            if step >= 1 and step <= 9:
-                if (str(maps[step-1]) not in "x0"):
-                    step_maps(maps,step,value) # Функция заполнения списка крестиком или ноликом,
-                                               # в зависимости от того что в нее передали.
-                    valid = True
-                else:
-                    print("Эта клеточка уже занята")
-                    continue
-            else:
-                    print("Некорректный ввод. Введите число от 1 до 9 чтобы сделать ход.")
-                    continue
-        rendering_list(maps)  # Показываем очередное текущее состояние поля клеток с учетом введенных ранее символов
+        valid_input(current_gamer, maps, value) #Функция проверки корректности ввода данных. В ней же функция заполнения
+                                                #списка крестиком или ноликом
+                               
+        rendering_list(maps)  # Функция рисования текущего состояния поля клеток с учетом введенных ранее символов  
         count += 1             
         number_of_cells -= count
         current_gamer = gamer_2 if current_gamer == gamer_1 else gamer_1
