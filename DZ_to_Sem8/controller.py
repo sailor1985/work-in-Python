ceb1d4a9d2393f7958ef24b7e2a940729b9aabca
Michael <cadetvire@rambler.ru>
2022-11-21 00:29:53 +0300

    Реализовал функцию обновления записи по ID

--- DZ_to_Sem8/controller.py
+++ DZ_to_Sem8/controller.py
@@ -1,7 +1,7 @@
 import model, view
 import csv
 
-# 1. Функция заполнения записями структуры хранения данных (словарь)
+# 1. CREATE: Функция заполнения записями структуры хранения данных (словарь)
 def add_records_in_dic() -> dict:
     quantity_records = view.quantity_records()
     structure = {}
@@ -11,51 +11,36 @@ def add_records_in_dic() -> dict:
         print("\n")
     return structure
 
-# dic_all = add_records_in_dic()
+# dic_all = add_records_in_dic(quantity_records)
 # view.view_data(dic_all)
-# dictionary = dic_all
+dictionary = {1: {'last_name': 'Гончаров', 'first_name': 'Михаил', 'class': '11А'}, 2: {'last_name': 'Петров', 'first_name': 'Сергей', 'class': '3В'}}
 
 #2. READ: Функция импорта из структуры хранения данных (словарь) записи по ID и вывода в консоль
 # с помощью textable
-def import_rec_from_dic_with_ID(dic_all: dict) -> dict:
-    ID = view.input_ID()
-    for key in dic_all.keys():
-        if key == ID:
-            val = dic_all[ID]
+def import_rec_from_dic_with_ID(db: dict) -> dict:
+    rec_ID = view.input_ID()
+    for key in db.keys():
+        if key == rec_ID:
+            val = db[rec_ID]
             model.rendering_dic_ID(val)
             break 
     return val
 
-# import_rec_from_dic_with_ID(dictionary)
+# import_rec_from_dic_with_ID(dic_all)
 
+#3. UPDATE: Обновить запись по ID
+def new_record_to_dic():
+    last_name, first_name, clas = view.add_record_surname(), view.add_record_name(), view.add_record_class()
+    dictionary = model.create_record(last_name, first_name, clas)
+    return dictionary
 
-# 2. Функция экспорта записей в файл и их вывода на экран с помощью Textable
-def add_records_in_file_print_with_textable():
-    records_lst = add_records_in_dic()
-    model.export_to_file("telephone directory.txt", records_lst, delimiter=",")
-    view.view_data("В файл записаны следующие записи:")
-    records_str = model.parsing_lst_for_table(records_lst)
-    records = records_str.split(",")
-    model.rendering_list(records)
-    return records_lst
+new_record = new_record_to_dic()
+view.view_data(new_record)
+rec_ID = view.input_ID()
 
+def update_record(db:dict, new_data: dict, rec_ID: int):
+    db[rec_ID] = new_data
+    return db
 
-# 3. Функция импорта записей из файла и их вывода на экран с помощью Textable
-def take_records_from_file_with_textable():
-    records_lst = model.import_from_file("telephone directory.txt")
-    view.view_data("Из файла cчитаны следующие записи:")
-    records_str = model.parsing_lst_for_table(records_lst)
-    records = records_str.split(",")
-    model.rendering_list(records)
-    return records_lst
-
-
-# 4. Функция добавления записей в файл с уже имеющимися записями и вывода на экран всех записей с учетом добавленных с помощью Textable
-def add_records_in_file_with_available_records_with_textable():
-    records_lst = add_records_in_dic()
-    model.rewrite_to_file("telephone directory.txt", records_lst, delimiter=",")
-    view.view_data("В файл дозаписаны следующие записи:")
-    records_str = model.parsing_lst_for_table(records_lst)
-    records = records_str.split(",")
-    model.rendering_list(records)
-    return records_lst
+update_value = update_record(dictionary, new_record, rec_ID)
+view.view_data(update_value)
