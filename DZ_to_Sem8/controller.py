b4c435800899198d21f9d849ae3780c577fd06cb
Michael <cadetvire@rambler.ru>
2022-11-22 23:13:30 +0300

    Сделал в разделе UPDATE вывод обновленного словаря через teaxtatble. Оформил вызов раздела CREATE в main

--- DZ_to_Sem8/controller.py
+++ DZ_to_Sem8/controller.py
@@ -1,36 +1,41 @@
 import model, view
 import csv
 
-# 1. CREATE: Функция заполнения записями структуры хранения данных (словарь)
-#quantity_records = view.quantity_records()
-def add_records_in_dic(quantity_records: int) -> dict:
+
+# 1. CREATE: Создаем запись по заданному множеству полей и записываем ее в БД. Поля вводятся пользователем.
+
+def add_records_in_dic() -> dict:
+    quantity_records = view.quantity_records()
     structure = {}
     for key in range(1, quantity_records + 1):
         last_name, first_name, clas = view.add_record_surname(), view.add_record_name(), view.add_record_class()
         structure[key] = model.create_record(last_name, first_name, clas)
         print("\n")
-    return structure
+    return view.view_data(structure)
 
-#dictionary = add_records_in_dic(quantity_records)
-#view.view_data(dictionary)
 
 # 2. WRITE: Экспорт в данных в csv файл и последнего ID словаря в файл Id.txt
-#model.export_to_file("list_of_students.csv", dictionary, delimiter=",")
+# model.export_to_file("list_of_students.csv", dictionary, delimiter=",")
+# model.export_ID_to_file("Id.txt", dictionary, delimiter=",")
 
 # На следующей строчке результат - CREATE: словарь dictionary. Раскомментировать строчку 21 для использования в READ,UPDATE,DELETE
-# dictionary = {1: {'last_name': 'Гончаров', 'first_name': 'Михаил', 'class': '11А'}, 2: {'last_name': 'Петров', 'first_name': 'Сергей', 'class': '3В'}}
+dictionary = {1: {'last_name': 'Гончаров', 'first_name': 'Михаил', 'class': '11А'},
+              2: {'last_name': 'Петров', 'first_name': 'Сергей', 'class': '3В'}}
 
-# 4. READ: Функция импорта из структуры хранения данных (словарь) записи по ID и вывода в консоль с помощью textable
+# 4. READ: Извлечение записи по ID из словаря и вывод в консоль с помощью textable
 # rec_ID = view.input_ID()
 # model.import_rec_from_dic_with_ID(dictionary, rec_ID)
+# if not rec_ID in dictionary:
+#     view.view_ID()
 
-# 5. UPDATE: Обновить запись по ID
-#last_name, first_name, clas = view.add_record_surname(), view.add_record_name(), view.add_record_class()
-#new_record = model.create_record(last_name, first_name, clas)
-#rec_ID = view.input_ID()
-#if rec_ID in dictionary:
-    #view.view_data(model.update_record(dictionary, new_record, rec_ID))
-#else: view.view_ID()
+# 5. UPDATE: Обновление записи по ID и вывод в консоль с помощью textable обновленного словаря
+# last_name, first_name, clas = view.add_record_surname(), view.add_record_name(), view.add_record_class()
+# new_record = model.create_record(last_name, first_name, clas)
+# rec_ID = view.input_ID()
+# if rec_ID in dictionary:
+# dic = model.update_record(dictionary, new_record, rec_ID)
+# model.rendering_list(dic)
+# else: view.view_ID()
 
 # 6. DELETE: Удалить запись по ID
 # rec_ID = view.input_ID()
@@ -38,24 +43,11 @@ def add_records_in_dic(quantity_records: int) -> dict:
 #     view.view_data(model.delete_record(dictionary, rec_ID))
 # else: view.view_ID()
 
-# 5. WRITE: Экспорт в csv файл
-def add_records_in_file_print_with_textable():
-    records_lst = add_records_in_dic()
-    model.export_to_file("list_of_students.csv", dictionary, delimiter=",")
-    view.view_data("В файл записаны следующие записи:")
-    records_str = model.parsing_lst_for_table(records_lst)
-    records = records_str.split(",")
-    model.rendering_list(records)
-    return records_lst
-
-
-
-#5. - Импорт из CSV файла с ID. Использовать пакет csv стандартной библиотеки.
+# 7. - Импорт из CSV файла с ID. Использовать пакет csv стандартной библиотеки.
 # Пример:
 # 1#Сидоров#Алексей#9А
 # 2#Соколов#Григорий#9А
 # Данные в БД добавляются, считаем, что уникальность реализуется с помощью ID.
 
-#csv.reader()
-
-# 2. Функция экспорта записей в файл и их вывода на экран с помощью Textable
+# 8. Импорт из CSV файла без ID
+# model.import_from_csv_without_ID("list_of_students.csv")
--- DZ_to_Sem8/model.py
+++ DZ_to_Sem8/model.py
@@ -1,4 +1,5 @@
 from texttable import Texttable
+import csv
 
 
 # Запись в текстовый файл. Вход: словарь
@@ -8,14 +9,13 @@ def export_to_file(filename: str, data: dict, delimiter=","):
             file.write(",".join(rec.values()))
             file.write(f"\n")
 
+
 # Запись в текстовый файл последнего ID словаря. Вход: словарь
 def export_ID_to_file(filename: str, data: dict, delimiter=","):
     with open(filename, mode="w", encoding="utf-8") as file:
         one, two = data.popitem()
         file.write(str(one))
 
-dictionary = {1: {'last_name': 'Гончаров', 'first_name': 'Михаил', 'class': '11А'}, 2: {'last_name': 'Петров', 'first_name': 'Сергей', 'class': '3В'}}
-export_ID_to_file("Id.txt", dictionary, delimiter=",")
 
 # Дозапись в текстовый файл. Вход:список
 def rewrite_to_file(filename: str, data: list, delimiter=","):
@@ -32,7 +32,7 @@ def import_from_file(filename: str) -> list:
     return a
 
 
-# Создание записи (Фамилия, Имя, Класс) в виде словаря
+# Функция cоздания записи (Фамилия, Имя, Класс) в виде словаря
 def create_record(last_name: str, first_name: str, clas: str) -> dict:
     dictionary = {'last_name': last_name, 'first_name': first_name, 'class': clas}
     return dictionary
@@ -42,10 +42,7 @@ def create_record(last_name: str, first_name: str, clas: str) -> dict:
 def import_rec_from_dic_with_ID(db: dict, rec_ID: int) -> dict:
     for key in db.keys():
         if key == rec_ID:
-            val = db[rec_ID]
-            rendering_dic_ID(val)
-            break
-    return val
+            rendering_dic_ID(db[rec_ID])
 
 
 # Обновление имеющейся записи словаря на новую (Фамилия, Имя, Класс)
@@ -60,22 +57,12 @@ def delete_record(db: dict, rec_ID: int):
     return db
 
 
-def parsing_lst(value_lst: list) -> str:  # Парсинг списка записей в строку
-    value_str = "\n".join(value_lst)
-    return value_str
-
-
-def parsing_lst_for_table(value_lst: list) -> str:  # Парсинг списка записей для таблицы Textable
-    value_str = ",".join(value_lst)
-    return value_str
-
-
-def rendering_list(dic: dict):  # Функция рисование таблицы со всеми записями, экспорт  ируемыми в файл/импортируемыми из файла
+def rendering_list(dic: dict):  # Функция рисование таблицы со всеми записями, экспортируемыми в файл/импортируемыми из файла
     table = Texttable()
-    value_lst = list(dic.values())
     maps = [["Фамилия", "Имя", "Класс"]]
-    for i in range(0, len(value_lst) - 1, 3):
-        maps.append([value_lst[i], value_lst[i + 1], value_lst[i + 2]])
+    for rec in list(dic.values()):
+        val = ",".join(rec.values()).split(",")
+        maps.append(val)
     table.add_rows(maps)
     print(table.draw())
 
@@ -87,3 +74,17 @@ def rendering_dic_ID(dic: dict):  # Функция рисование табли
     maps = [["last_name", "first_name", "clas"], value_lst[:3]]
     table.add_rows(maps)
     print(table.draw())
+
+
+def import_from_csv_without_ID(filename: str):  # Функция импорта из csv файла (без ID)
+    with open(filename, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(','.join(row))
+
+
+def import_from_csv_with_ID(filename: str):  # Функция импорта из csv файла (c ID)
+    with open(filename, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(','.join(row))
