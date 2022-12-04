e3e40970f041196d8abd00d2a1eae97255758aec
Michael <cadetvire@rambler.ru>
2022-12-05 00:49:22 +0300

    Построение графика по отфильтрованным значениям

--- Sem11/main.py
+++ Sem11/main.py
@@ -13,7 +13,10 @@ dfATA = df[df["Operating Airline"] == "ATA Airlines"]
 dfAC = df[df["Operating Airline"] == "Air Canada"]
 dfAWA = df[df["Operating Airline"] == "WestJet Airlines"]
 
+ds_date = pd.to_datetime(dfATA.activity_Period, format = "%Y%m")
 #2. Построение графика по числовым значениям + по отфильтрованным значениям
 fig, ax = plt.subplots()
-plt.plot(dfATA.activity_Period, dfATA["Passenger Count"])
+plt.plot(pd.to_datetime(dfATA.activity_Period, format = "%Y%m"), dfATA["Passenger Count"], "-*k")
+plt.plot(pd.to_datetime(dfAC.activity_Period, format = "%Y%m"), dfAC["Passenger Count"], "-or")
+plt.plot(pd.to_datetime(dfAWA.activity_Period, format = "%Y%m"), dfAWA["Passenger Count"], "-b")
 plt.show()
