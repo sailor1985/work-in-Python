ab0fc006a527ebf2dfea771f5420a86620ffd2dd
Michael <cadetvire@rambler.ru>
2022-12-06 19:52:22 +0300

    Сделал функцию суммирования чисел через команду в боте

--- DZ_to_Sem10/bot_coomands.py
+++ DZ_to_Sem10/bot_coomands.py
@@ -1,17 +1,20 @@
 from telegram import Update
-# from telegram.ext import Updater, CommandHandler, CallbackContext
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ContextTypes
 import datetime
 
 
-async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Hi {update.effective_user.first_name}!')
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'/hi\n/time\n/help')
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'/hi\n/time\n/help\n/sum')
 
-async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
 
-async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'{datetime.datetime.now().time()}')
+async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    items = msg.split()
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} + {y} = {x+y}')
--- DZ_to_Sem10/main.py
+++ DZ_to_Sem10/main.py
@@ -1,13 +1,12 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler
 from bot_coomands import *
-
 
 app = ApplicationBuilder().token("5834653879:AAHe6BnTUvJ2ZpxLhNjbFdj0VmV8u1pkUb4").build()
 
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("time", time_command))
 app.add_handler(CommandHandler("help", help_command))
-
+app.add_handler(CommandHandler("sum", sum_command))
 print("Server start")
+
 app.run_polling()
